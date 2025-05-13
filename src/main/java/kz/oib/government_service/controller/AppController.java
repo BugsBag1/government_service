df0c2b74e24b6f19aacc295bf14abe95c60753f6
BugsBag1 <kbk.bagi01@gmail.com>
2025-05-14 03:03:19 +0600

    first commit

---
+++ src/main/java/kz/oib/government_service/controller/AppController.java
@@ -0,0 +1,11 @@
+package kz.oib.government_service.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequestMapping(value = "government-service")
+public class AppController {
+}
---
+++ src/test/java/kz/oib/government_service/GovernmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.oib.government_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GovernmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
